557f09b53655fe4dc6b4271ccf7b23d91eee9d14
Jordi <jjordihm9@gmail.com>
2021-08-05 22:27:39 +0200

    changed hangman from images to pure html & css

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 
 const Hangman = ({ failures }) => {
-    /**
-     * Depending on the number of failures loads the image of the status of the hangman
-     * @returns path from the image 
-     */
-    function imagePath() {
-        return `/images/hangman-${failures}.svg`;
-    }
+    // /**
+    //  * Depending on the number of failures loads the image of the status of the hangman
+    //  * @returns path from the image 
+    //  */
+    // function imagePath() {
+    //     return `/images/hangman-${failures}.svg`;
+    // }
+
+    // return (
+    //     <div id="hangman">
+    //         <img src={imagePath()} alt="hangman" />
+    //     </div>
+    // );
 
     return (
         <div id="hangman">
-            <img src={imagePath()} alt="hangman" />
+            <div className={`hangman base ${failures >= 1 ? 'show' : ''}`}></div>
+            <div className={`hangman stick ${failures >= 2 ? 'show' : ''}`}></div>
+            <div className={`hangman top ${failures >= 3 ? 'show' : ''}`}></div>
+            <div className="hangman man">
+                <div className={`hangman rope ${failures >= 4 ? 'show' : ''}`}></div>
+                <div className={`hangman head ${failures >= 5 ? 'show' : ''}`}></div>
+                <div className={`hangman body ${failures >= 6 ? 'show' : ''}`}></div>
+                <div className={`hangman arm right ${failures >= 7 ? 'show' : ''}`}></div>
+                <div className={`hangman arm left ${failures >= 8 ? 'show' : ''}`}></div>
+                <div className={`hangman leg right ${failures >= 9 ? 'show' : ''}`}></div>
+                <div className={`hangman leg left ${failures >= 10 ? 'show' : ''}`}></div>
+            </div>
         </div>
     );
 }
