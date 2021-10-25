3faa3b57a07c66af3c8699dafc1626ca116d1092
Jordi Hernandez <jjordihm9@gmail.com>
2021-10-25 18:36:43 +0200

    fix tabindex typo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ children, action, tabIndex = null }) => {
     return (
-        <button className="btn" onClick={action} tabindex={tabIndex}>
+        <button className="btn" onClick={action} tabIndex={tabIndex}>
             {children}
         </button>
     );
